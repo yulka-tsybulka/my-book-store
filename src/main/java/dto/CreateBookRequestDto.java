832d3fe7a181771bd2f3b8b42fced70362402f87
DOM <kasinets.ulia@gmail.com>
2023-11-07 14:41:25 +0200

    fixed mistakes

--- src/main/java/dto/CreateBookRequestDto.java
+++ src/main/java/dto/CreateBookRequestDto.java
@@ -1,23 +1,18 @@
 package dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Column;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CreateBookRequestDto {
-    @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
     private String author;
 
-    @Column(unique = true, nullable = false)
     private String isbn;
 
-    @Column(nullable = false)
     private BigDecimal price;
 
     private String description;
