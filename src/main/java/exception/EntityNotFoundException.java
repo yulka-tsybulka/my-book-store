289d23cb2ee46d72dad41f0c41e234f11908dcdf
DOM <kasinets.ulia@gmail.com>
2023-12-20 18:33:24 +0200

    fixed mistake

--- src/main/java/exception/EntityNotFoundException.java
+++ src/main/java/exception/EntityNotFoundException.java
@@ -1,7 +1,5 @@
 package exception;
 
-import java.util.function.Supplier;
-
 public class EntityNotFoundException extends RuntimeException {
     public EntityNotFoundException(String message) {
         super(message);
--- src/main/java/exception/RegistrationException.java
+++ src/main/java/exception/RegistrationException.java
@@ -1,7 +1,5 @@
 package exception;
 
-import java.util.function.Supplier;
-
 public class RegistrationException extends RuntimeException {
     public RegistrationException(String message) {
         super(message);
